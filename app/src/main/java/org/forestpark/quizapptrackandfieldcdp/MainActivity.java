7ae75204f4d965c3d04e7dcf3f677d5f3061e620
Payne <cdpayne@bcps.k12.md.us>
2023-07-25 10:26:28 -0400

    Added a score button and calculate score

--- app/src/main/java/org/forestpark/quizapptrackandfieldcdp/MainActivity.java
+++ app/src/main/java/org/forestpark/quizapptrackandfieldcdp/MainActivity.java
@@ -2,6 +2,8 @@ package org.forestpark.quizapptrackandfieldcdp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
     TextView questionTV;
     Button falseBTN;
     Button trueBTN;
+    Button scoreBTN;
+
+    int score;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
         questionTV = (TextView) findViewById(R.id.questionTV);
         falseBTN = (Button) findViewById(R.id.falseBTN);
         trueBTN = (Button) findViewById(R.id.trueBTN);
+        scoreBTN = (Button) findViewById(R.id.scoreBTN);
+        score = 0;
 
         falseBTN.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,9 +46,17 @@ public class MainActivity extends AppCompatActivity {
             public void onClick (View view){
                 CharSequence text = "You are correct! Congrats!";
                 int duration = Toast.LENGTH_SHORT;
-
+                score++;
                 Toast toast = Toast.makeText(getApplicationContext(), text, duration);
                 toast.show();
+            }
+        });
+        scoreBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              Intent scoreIntent = new Intent(MainActivity.this, ScoreActivity.class);
+              scoreIntent.putExtra("score", score);
+              startActivity(scoreIntent);
             }
         });
     }
