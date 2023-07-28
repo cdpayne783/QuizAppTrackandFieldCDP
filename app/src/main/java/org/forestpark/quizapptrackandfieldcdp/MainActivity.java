a19de3898e37ec9f1df32a69b1dfa8e9291b0e9d
Payne <cdpayne@bcps.k12.md.us>
2023-07-28 12:48:58 -0400

    add switch button to change bckgd color

--- app/src/main/java/org/forestpark/quizapptrackandfieldcdp/MainActivity.java
+++ app/src/main/java/org/forestpark/quizapptrackandfieldcdp/MainActivity.java
@@ -1,16 +1,30 @@
 package org.forestpark.quizapptrackandfieldcdp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.content.SharedPreferences;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
+
 
 public class MainActivity extends AppCompatActivity {
+    
+    private View parentView;
+    private Switch themeSwitch;
+    TextView themeTV, titleTV;
+    private UserSettings settings;
 
     TextView questionTV;
     Button falseBTN;
@@ -27,7 +41,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
+        settings = (UserSettings) getApplication();
+        
+        
+        intiWidgets();
+        loadSharedPreferences();
+        initSwitchListener();
         questionTV = (TextView) findViewById(R.id.questionTV);
         falseBTN = (Button) findViewById(R.id.falseBTN);
         trueBTN = (Button) findViewById(R.id.trueBTN);
@@ -47,14 +67,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (currentQ.getCorrectAnswer() == false)
                 {
-                 message = getString(R.string.rightMSG);
-                 score++;
+                    message = getString(R.string.rightMSG);
+                    score++;
                     MediaPlayer music = MediaPlayer.create(MainActivity.this, R.raw.cheer);
                     music.start();
                 }
                 else
                 {
-                message = getString(R.string.wrongMSG);
+                    message = getString(R.string.wrongMSG);
                     MediaPlayer music = MediaPlayer.create(MainActivity.this, R.raw.missed);
                     music.start();
                 }
@@ -89,23 +109,88 @@ public class MainActivity extends AppCompatActivity {
         });
         nextBTN.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-              currentIndex++;
-              if(currentIndex <= 5)
-              {
-                  currentQ = questions[currentIndex];
-                  questionTV.setText(currentQ.getqPrompt());
+            public void onClick (View view) {
+                currentIndex++;
+                if(currentIndex <= 5)
+                {
+                    currentQ = questions[currentIndex];
+                    questionTV.setText(currentQ.getqPrompt());
 
-                  MediaPlayer music = MediaPlayer.create(MainActivity.this, currentQ.getSound());
-                  music.start();
-              }
-              else
-              {
-                  Intent scoreIntent = new Intent(MainActivity.this, ScoreActivity.class);
-                  scoreIntent.putExtra("score", score);
-                  startActivity(scoreIntent);
-              }
+                    MediaPlayer music = MediaPlayer.create(MainActivity.this, currentQ.getSound());
+                    music.start();
+                }
+                else
+                {
+                    Intent scoreIntent = new Intent(MainActivity.this, ScoreActivity.class);
+                    scoreIntent.putExtra("score", score);
+                    startActivity(scoreIntent);
+                }
+            }
+        });
+     }
+
+    private void intiWidgets() {
+        themeTV = findViewById(R.id.themeTV);
+        titleTV = findViewById(R.id.titleTV);
+        themeSwitch = findViewById(R.id.themeSwitch);
+        parentView = findViewById(R.id.parentView);
+    }
+
+    private void loadSharedPreferences()
+    {
+        SharedPreferences sharedPreferences = getSharedPreferences(UserSettings.PREFERENCES, MODE_PRIVATE);
+        String theme = sharedPreferences.getString(UserSettings.CUSTOM_THEME, UserSettings.LIGHT_THEME);
+        settings.setCustomTheme(theme);
+        updateView();
+    }
+
+    private void initSwitchListener()
+    {
+        themeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean checked)
+            {
+                if(checked)
+                    settings.setCustomTheme(UserSettings.DARK_THEME);
+                else
+                    settings.setCustomTheme(UserSettings.LIGHT_THEME);
+
+                SharedPreferences.Editor editor = getSharedPreferences(UserSettings.PREFERENCES, MODE_PRIVATE).edit();
+                editor.putString(UserSettings.CUSTOM_THEME, settings.getCustomTheme());
+                editor.apply();
+                updateView();
             }
         });
     }
-}
+
+    private void updateView()
+    {
+        final int black = ContextCompat.getColor(this, R.color.black);
+        final int white = ContextCompat.getColor(this, R.color.white);
+
+        if(settings.getCustomTheme().equals(UserSettings.DARK_THEME))
+        {
+            titleTV.setTextColor(white);
+            themeTV.setTextColor(white);
+            themeTV.setText("Dark");
+            parentView.setBackgroundColor(black);
+            themeSwitch.setChecked(true);
+        }
+        else
+        {
+            titleTV.setTextColor(black);
+            themeTV.setTextColor(black);
+            themeTV.setText("Light");
+            parentView.setBackgroundColor(white);
+            themeSwitch.setChecked(false);
+        }
+
+    }
+
+
+
+
+
+    }
+
